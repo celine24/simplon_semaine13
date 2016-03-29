d41c9407f5521d72f46392b424a4e1e641616e89
celine24 <peyrot.celine@gmail.com>
2016-03-29 11:00:26 +0000

    etape 1

--- cpt/js/controller/count.js
+++ cpt/js/controller/count.js
@@ -1,6 +1,19 @@
-app.controller('appCtrl', ['$scope', '$http', function ($scope, $http) {
+app.controller('appCtrl', ['$scope', '$http', function ($scope, $http, $rootScope) {
     $scope.depenses = [
         { valeur: 50 },
         { valeur: 25 }
     ];
+    
+    $scope.addExp = function(expense) {
+        var obj = {};
+        expense = parseInt(expense, 10);
+        if (!isNaN(expense)) {
+            obj["valeur"] = expense; 
+            $scope.depenses.push(obj);
+        }
+        else {
+            alert("Vous n\'avez pas entré un nombre.");
+        }
+    };
+    
 }]);
